3503eb58dfd09f7562a7606224373bdb8468135e
EzgiBerraDemirel <ezgiberrademirel@gmail.com>
2024-01-22 22:04:08 +0300

    StayHub
    rezervasyon web sitesi

---
+++ airbnb/client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#c026d3',
+        mavi:'#f3e8ff',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
